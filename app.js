681fe2dcbf65a0073ae86ffb6540bb1a9e9596ec
Anand-Varma-838 <50435468+Anand-Varma-838@users.noreply.github.com>
2021-06-19 23:04:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const request = require("request");
 const path = require('path');
 const { json } = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 app.use(parser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname,'public')))
@@ -48,6 +49,6 @@ app.post('/', function(req, res){
     });
   });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(port, function () {
   console.log('Node app is working!');
 });
